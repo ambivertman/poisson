bf5d0c5e0f8af913a8abb6c498e80889523e3fea
ambivertman <72485229+ambivertman@users.noreply.github.com>
2021-12-26 12:05:16 +0800

    Update main.c

--- main.c
+++ main.c
@@ -4,38 +4,36 @@
 #include <unistd.h>
 #include <math.h>
 #include "poisson.h"
-double mean(double a[100]); //定义mean 函数,用该函数计算k值在	10000个数据中出现的频率，即概率
-int search(int a[100],int x); //定义search函数,用该函数计算数值k在随机生成的100个数中出现的次数
-double function(double prob); //定义function函数,用该函数判断是否满足泊松分布
-double fact(int k);//定义fact函数，用该函数计算k的阶乘
 
 
 int main(void)
 {
     int i,j,k,count,random[100];//定义循环变量i和j,整数型变量count，随机数组random[100]
     double p[100],prob;
-    printf("请输入k值");//k为X可能的取值
+    printf("请输入k值:");//k为X可能的取值
     scanf("%d",&k);
     for(j=0; j<100; j++)
     {
+        srand((unsigned)time(NULL));
+        printf("第%d组随机生成0~100的数: \n",j+1);
         for (i = 0; i < 100; i++)
         {
             int num = rand()%100+1;//对100取余操作
             random[i]=num;
-            printf("第%d次随机生成0~100的数: %d \n",i+1,random[i]);
+            printf("% 4d",random[i]);
+            if((i+1)%10==0) printf("\n");
         }
-        count=search(random,k);
-         printf("出现次数为%d\n",count);//输出数值k在随机生成的100个数中出现的次数
+        count=lsearch(random,k);
         p[j]=count/100.0;
-        printf("%d出现的频率为%lf\n",k,p[j]); //输出数值k在随机生成的100个数中出现的频率
-
-          printf("\n");
+        printf("%d在第%d组中的概率为%.2lf\n",k,j+1,p[j]); //输出数值k在随机生成的100个数中出现的频率
+        printf("\n");
     }
     printf("\n");
     prob=mean(p);//调用mean函数
-    printf("%d出现的概率为:%lf",k,prob);//输出k值在10000个数据中出现的频率，即概率
+    printf("%d出现的概率为:%.4lf",k,prob);//输出k值在10000个数据中出现的频率，即概率
     printf("\n");
-    function(prob);//调用函数function
+    func(prob);//调用函数function
+
     return 0;
 }
 
